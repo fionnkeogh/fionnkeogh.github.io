3d0d9e168e9667f99b2ebebb111de2d0c0d9cef2
Fionn Keogh <fionndaire.keogh@outlook.de>
2024-01-06 23:02:06 +0100

    Update main.py

--- scam/python/main.py
+++ scam/python/main.py
@@ -1,9 +1,4 @@
 import random
-import pyodide_js
-await micropip.install("numpy")
-await micropip.install("scipy")
-await pyodide_js.loadPackage('numpy')
-await pyodide_js.loadPackage('scipy')
 from python.simulation import Simulation, Macrophage, Candida, Cytokine, Player
 # from python.grid import Grid
 # from python.cytokine import Cytokine
